01f8dd0845d416d2fe62a17de23d22baef1733e4
Anand Balaji <76686299+anand-2@users.noreply.github.com>
2024-03-31 17:56:55 +0530

    home url changed

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
       return [
         {
           source: '/',
-          destination: '/home',
+          destination: '/dashboard',
           permanent: true,
         },
       ];
