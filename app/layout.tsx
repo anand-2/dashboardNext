ef8477e8ccdc0e0d523bd5dac6f31794c51e28d1
Anand Balaji <76686299+anand-2@users.noreply.github.com>
2024-04-01 17:11:17 +0530

    title chaged

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,7 @@ import '@mantine/core/styles.css';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 
 export const metadata = {
-  title: 'My Mantine app',
-  description: 'I have followed setup instructions carefully',
+  title: 'Dashboard',
 };
 
 export default function RootLayout({
