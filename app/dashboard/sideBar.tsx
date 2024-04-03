13ef6bddfde81714ff86803defcccd89be727f49
Anand Balaji <76686299+anand-2@users.noreply.github.com>
2024-04-03 12:09:06 +0530

    dashboard responsive test

--- app/dashboard/sideBar.tsx
+++ app/dashboard/sideBar.tsx
@@ -1,6 +1,9 @@
 'use client'
-import { useState } from 'react';
-import { Group, Code,Divider } from '@mantine/core';
+import { useState,useEffect } from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import { Group, Code,Divider, DrawerBody } from '@mantine/core';
+import { Drawer, Button } from '@mantine/core';
+
 import {
   IconBellRinging,
   IconFingerprint,
@@ -29,12 +32,14 @@ const data = [
 
  function SideBar() {
   const [active, setActive] = useState('Dashboard');
+  const [opened, setOpened] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+
 
   const links = data.map((item) => (
     <a
       className={classes.link}
-      style={{backgroundColor : active === item.label ? '#5CB360' : ''}}
-      
+      style={{backgroundColor : active === item.label ? '#5CB360' : ''}}      
       data-active={item.label === active || undefined}
       href={item.link}
       key={item.label}
@@ -48,26 +53,58 @@ const data = [
     </a>
   ));
 
+  const handleClose = () => {
+    setOpened(false);
+  };
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+      if (isSmallScreen) {
+        setOpened(false); // Close drawer if screen is small
+      }
+    };
+
+    console.log(!isSmallScreen,window.innerWidth,"small")
+    window.addEventListener('resize', handleResize);
+
+     }, [window.innerWidth]);
+
   return (
-    <nav className={classes.navbar}>
-      <div className={classes.navbarMain}>
-        <Group className={classes.header} >
-        <IconLayoutDashboardFilled className={classes.headerIcon} />
-        <h1 style={{color:'white'}}>Dashboard</h1>                 
-        </Group>
-        <Divider my="xs" mb='1rem' />
-
-        {links}
-        <div  className={classes.footer}>
-               <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
-          <IconLogout className={classes.linkIcon} stroke={1.5} />
-          <span>Logout</span>
-        </a>
-      </div>
-      </div>
-
+    <Drawer
+    opened={!isSmallScreen}
+    size='16rem'
+    onClose={handleClose}
+     padding="0px"
+    className='drawer'
+    withCloseButton={false} 
+    withOverlay={false}
+    closeOnClickOutside={true}
     
-    </nav>
+  >
+    
+    <nav className={classes.navbar}>
+    <div className={classes.navbarMain}>
+      <Group className={classes.header} >
+      <IconLayoutDashboardFilled className={classes.headerIcon} />
+      <h1 style={{color:'white'}}>Dashboard</h1>                 
+      </Group>
+      <Divider my="xs" mb='1rem' />
+
+      {links}
+      <div  className={classes.footer}>
+             <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+        <IconLogout className={classes.linkIcon} stroke={1.5} />
+        <span>Logout</span>
+      </a>
+    </div>
+    </div>
+
+  
+  </nav>
+    </Drawer>
+    
   );
 }
 
